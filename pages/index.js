6103a5a64ebe2c8c58bfc5bf6d207996f552a242
Anders Kjær Damgaard <akd@akd.io>
2019-01-04 16:21:58 +0100

    Minimal viable product

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
-export default () => <div>Hello world!</div>;
+import Head from "next/head";
+import "./index.scss";
+
+export default () => (
+  <>
+    <Head>
+      <title>akd.io</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    </Head>
+    <div className="card">
+      <img src="static/picture.png" />
+      <h1 className="name">Anders Kjær Damgaard</h1>
+      <h2 className="position">Software Developer</h2>
+      <h3 className="location">Copenhagen, Denmark</h3>
+      <p className="description">
+        Software development and computer science is an old, long-lasting passion of mine. A passion
+        that my curiosity has always led me to, and one that I am currently enjoying, doing frontend
+        development using modern web technologies.
+      </p>
+      <div className="social-media">
+        <a href="https://www.linkedin.com/in/anders-damgaard/">LinkedIn</a>
+        <a href="https://github.com/akd-io">GitHub</a>
+        <a href="mailto:akd@akd.io">Email</a>
+      </div>
+    </div>
+  </>
+);
