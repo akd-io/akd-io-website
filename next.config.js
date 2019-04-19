c42a09d069dbd48b36c775a271b83a5488fce91b
Anders Kjær Damgaard <akd@akd.io>
2019-04-19 21:49:40 +0200

    Remove unused parameters from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js heavily inspired by https://github.com/zeit/next.js/issues/5750#issuecomment-442313585 to fix now-deployment bug.
 
-module.exports = (phase, { defaultConfig }) => {
+module.exports = () => {
   const withSass = require('@zeit/next-sass');
   return withSass({
     target: 'serverless',
