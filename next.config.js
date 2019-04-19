928ab6b095e20c99451513b79a33d77461c26253
Anders Kjær Damgaard <akd@akd.io>
2019-04-19 21:53:49 +0200

    Remove irrelevant comment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// next.config.js heavily inspired by https://github.com/zeit/next.js/issues/5750#issuecomment-442313585 to fix now-deployment bug.
-
 module.exports = () => {
   const withSass = require('@zeit/next-sass');
   return withSass({
