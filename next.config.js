4e27ab1486fd7ff55fda49b8c015ffbf7cc4d045
Anders Kjær Damgaard <akd@akd.io>
2019-04-19 21:47:47 +0200

    Add serverless target, and delete old unnecessary bug-fix from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 // next.config.js heavily inspired by https://github.com/zeit/next.js/issues/5750#issuecomment-442313585 to fix now-deployment bug.
 
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === "development"
-    ? {} // We're never in "production server" phase when in development mode
-    : !process.env.NOW_REGION
-    ? require("next/constants") // Get values from `next` package when building locally
-    : require("next-server/constants"); // Get values from `next-server` package when building on now v2
-
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Config used to run in production.
-    return {};
-  }
-
-  const withSass = require("@zeit/next-sass");
-
-  return withSass();
+  const withSass = require('@zeit/next-sass');
+  return withSass({
+    target: 'serverless',
+  });
 };
