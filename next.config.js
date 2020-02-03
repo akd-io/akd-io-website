782781d35f8b9f812b66a9929baa7074ca129a8f
Anders Damgaard <akd@akd.io>
2020-02-03 17:41:51 +0100

    Remove @zeit/next-css, @zeit/next-sass and node-sass, as well as its usage in next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  const withCSS = require('@zeit/next-css');
-  const withSass = require('@zeit/next-sass');
-  return withCSS(
-    withSass({
-      target: 'serverless',
-    })
-  );
-};
