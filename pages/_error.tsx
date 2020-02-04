e50c01f69b3f0762e42d5ec3f75887ccabd7de34
Anders Damgaard <akd@akd.io>
2020-02-04 17:54:13 +0100

    Style Error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,32 +1,53 @@
+import styled from '@emotion/styled';
 import Error from 'next/error';
 import Link from 'next/link';
 import React from 'react';
 import PageWrapperWithoutMetadata from '../components/private/PageWrapperWithoutMetadata';
 
+const ErrorContainer = styled.main`
+  min-height: 100vh;
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+  text-align: center;
+`;
+
+const P = styled.p`
+  text-align: center;
+  margin: 0;
+  margin-bottom: 10px;
+`;
+
 class MyError extends Error {
   render = (): JSX.Element => {
-    const { statusCode } = this.props;
+    //    const { statusCode } = this.props;
+    const statusCode: number = 500;
 
     let title = 'Error';
-    let header = 'An error occured';
+    let header = <H1>An error occured</H1>;
     let backToFrontPageParagraph = (
-      <>
+      <P>
         Back to the{' '}
         <Link href="/">
           <a>front page</a>
         </Link>
         ?
-      </>
+      </P>
     );
     let tryAgainParagraph = (
-      <>
+      <P>
         Please try again later, or <a href="mailto:akd@akd.io">contact me</a> if the error persists.
-      </>
+      </P>
     );
     let paragraph = (
       <>
         {tryAgainParagraph}
-        <br />
         {backToFrontPageParagraph}
       </>
     );
@@ -34,21 +55,23 @@ class MyError extends Error {
     switch (statusCode) {
       case 404: {
         title = 'Page not found';
-        header = 'Page not found';
+        header = <H1>Page not found</H1>;
         paragraph = backToFrontPageParagraph;
         break;
       }
       case 500: {
         title = 'Internal server error';
-        header = 'Internal server error';
+        header = <H1>Internal server error</H1>;
         break;
       }
     }
 
     return (
       <PageWrapperWithoutMetadata title={title}>
-        <h1>{header}</h1>
-        <p>{paragraph}</p>
+        <ErrorContainer>
+          {header}
+          {paragraph}
+        </ErrorContainer>
       </PageWrapperWithoutMetadata>
     );
   };
