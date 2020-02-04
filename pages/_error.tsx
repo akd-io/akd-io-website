dd469785cf6c29c0ca36af9b5596aa42db10d2b8
Anders Damgaard <akd@akd.io>
2020-02-04 15:37:00 +0100

    Refactor skeleton

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,37 +1,57 @@
 import Error from 'next/error';
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 class MyError extends Error {
-  render = (): JSX.Element => (
-    <>
-      <Head>
-        <title>Error {this.props.statusCode}</title>
-      </Head>
-      {this.props.statusCode === 500 ? (
-        <>
-          <h1>Internal server error</h1>
-          <p>
-            Please try again later, or <a href="mailto:akd@akd.io">contact me</a> if the error
-            persists.
-          </p>
-        </>
-      ) : this.props.statusCode === 404 ? (
-        <>
-          <h1>Page not found</h1>
-          <p>Back to the front page?</p>
-        </>
-      ) : (
-        <>
-          <h1>An error occured</h1>
-          <p>
-            Please try again later, or <a href="mailto:akd@akd.io">contact me</a> if the error
-            persists.
-          </p>
-        </>
-      )}
-    </>
-  );
+  render = (): JSX.Element => {
+    const { statusCode } = this.props;
+
+    let title = 'Error';
+    let header = 'An error occured';
+    let paragraph = (
+      <>
+        Please try again later, or <a href="mailto:akd@akd.io">contact me</a> if the error persists.
+        <br />
+        Back to the{' '}
+        <Link href="https://akd.io">
+          <a>front page</a>
+        </Link>
+        ?
+      </>
+    );
+    switch (statusCode) {
+      case 404: {
+        title = 'Page not found';
+        header = 'Page not found';
+        paragraph = (
+          <>
+            Back to the{' '}
+            <Link href="https://akd.io">
+              <a>front page</a>
+            </Link>
+            ?
+          </>
+        );
+        break;
+      }
+      case 500: {
+        title = 'Internal server error';
+        header = 'Internal server error';
+        break;
+      }
+    }
+    return (
+      <>
+        <Head>
+          <title>{title}</title>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <h1>{header}</h1>
+        <p>{paragraph}</p>
+      </>
+    );
+  };
 }
 
 export default MyError;
