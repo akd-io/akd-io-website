14808e77de9ad2740ae59d6ed2b475c66e33020e
Anders Kjær Damgaard <akd@trifork.com>
2020-05-25 12:33:32 +0200

    Update Plausible script

--- components/private/BasePageWrapper.tsx
+++ components/private/BasePageWrapper.tsx
@@ -8,7 +8,7 @@ const BasePageWrapper: FC<PropsWithChildren<{}>> = ({ children }) => (
       <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-      <script async defer data-domain="akd.io" src="https://plausible.io/js/plausible.js" />
+      <script async defer data-domain="akd.io" src="https://stats.akd.io/js/index.js" />
     </Head>
     <GlobalStyles />
     {children}
