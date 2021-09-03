3855e79fc1972841bad94ade8e083acd9d672c74
Anders Damgaard <akd@akd.io>
2021-09-03 13:50:53 +0200

    Disable react/react-in-jsx-scope rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,9 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
