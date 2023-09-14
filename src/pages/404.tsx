3ba7172817df75b03cf0331e0202695eeedc9bc9
Anders Kjær Damgaard <akd@akd.io>
2023-09-14 19:49:54 +0200

    Update 404 and 500 pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,10 +4,11 @@ import PageWrapperWithoutMetadata from "../components/PageWrapperWithoutMetadata
 
 const Page404: FC = () => (
   <PageWrapperWithoutMetadata title="Page not found">
-    <div>
-      <h1>Page not found</h1>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-3 px-4 py-12">
+      <span className="text-7xl font-bold opacity-30">404</span>
+      <h1 className="text-xl font-bold">Page not found.</h1>
       <p>
-        Back to the <Anchor href="/">front page</Anchor>.
+        Let{"'"}s get you <Anchor href="/">back to safety</Anchor>.
       </p>
     </div>
   </PageWrapperWithoutMetadata>
