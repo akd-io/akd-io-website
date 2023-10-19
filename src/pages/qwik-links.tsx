72920e800cfd136f3cd9cb1bcfd945024857c3b4
Anders Kjær Damgaard <akd@akd.io>
2023-10-19 12:52:20 +0200

    Update links

--- src/pages/qwik-links.tsx
+++ src/pages/qwik-links.tsx
@@ -8,7 +8,17 @@ import { NextPage } from "next"
 const Index: NextPage = () => {
   const links = [
     { title: "Qwik Website", href: "https://qwik.builder.io/" },
+    { title: "Qwik Tutorial", href: "https://qwik.builder.io/tutorial/" },
+    { title: "Qwik Sandbox", href: "https://qwik.builder.io/examples/introduction/hello-world/" },
     { title: "Builder Blog", href: "https://www.builder.io/blog" },
+    {
+      title: "Builder Blog: Hydration is Pure Overhead",
+      href: "https://www.builder.io/blog/hydration-is-pure-overhead",
+    },
+    {
+      title: "Builder Blog: Hydration, the Saboteur of Lazy Loading",
+      href: "https://www.builder.io/blog/hydration-sabotages-lazy-loading",
+    },
     { title: "Qwik Docs", href: "https://qwik.builder.io/docs/" },
     { title: "Qwik Docs: Think Qwik", href: "https://qwik.builder.io/docs/concepts/think-qwik/" },
     { title: "Qwik Docs: Resumable", href: "https://qwik.builder.io/docs/concepts/resumable/" },
@@ -21,6 +31,10 @@ const Index: NextPage = () => {
     { title: "Mitosis GitHub", href: "https://github.com/BuilderIO/mitosis" },
     { title: "Mitosis playground", href: "https://mitosis.builder.io/" },
     { title: "Builder.io", href: "https://builder.io/" },
+    {
+      title: "Builder Blog: Introducing Visual Copilot: A Better Figma-to-Code Workflow",
+      href: "https://www.builder.io/blog/figma-to-code-visual-copilot",
+    },
   ]
   return (
     <PageWrapperWithMetadata
@@ -48,18 +62,20 @@ const Index: NextPage = () => {
           <section className="flex max-w-lg flex-col gap-3">
             <h2 className="text-xl font-bold">Hello! 👋</h2>
             <p>
-              {`On October 19, 2023 I held a talk at CopenhagenJS titled "Qwik and Resumability". This page compiles a list of useful links for the attendees to get started with Qwik.`}
+              {`On October 19, 2023 I held a talk at CopenhagenJS titled "Qwik & Resumability". This page compiles a list of useful links for the attendees to get started with Qwik.`}
             </p>
-            <p>
-              {`If you didn't attend the talk, or are interested anyway, check out the `}
-              <Anchor
-                href="https://docs.google.com/presentation/d/e/2PACX-1vRVlmQNmpwlSNWUgpTe_l3nMrR0W4S55VIknSfLWN6ghMK7bOJ5xd63VAJo-L5Br1JVZnKOIwxJw3AA/pub?start=false&loop=false&delayms=3000"
-                isExternal
-              >
-                Qwik & Resumability slides
-              </Anchor>
-              .
-            </p>
+          </section>
+          <section className="flex max-w-lg flex-col gap-3">
+            <h2 className="text-xl font-bold">Slides</h2>
+            <Anchor
+              href="https://docs.google.com/presentation/d/e/2PACX-1vRVlmQNmpwlSNWUgpTe_l3nMrR0W4S55VIknSfLWN6ghMK7bOJ5xd63VAJo-L5Br1JVZnKOIwxJw3AA/pub?start=false&loop=false&delayms=3000"
+              isExternal
+              target="_blank"
+            >
+              Qwik & Resumability slides
+            </Anchor>
+          </section>
+          <section className="flex max-w-lg flex-col gap-3">
             <h2 className="text-xl font-bold">Links</h2>
             <ul className="flex list-disc flex-col gap-2 pl-6">
               {links.map((link) => (
@@ -75,13 +91,19 @@ const Index: NextPage = () => {
             <h2 className="text-xl font-bold">Follow me online</h2>
             <div className="flex gap-3">
               <Button variant="outline" asChild>
-                <a href="https://twitter.com/akd_io">Twitter</a>
+                <a href="https://twitter.com/akd_io" target="_blank">
+                  Twitter
+                </a>
               </Button>
               <Button variant="outline" asChild>
-                <a href="https://github.com/akd-io">GitHub</a>
+                <a href="https://github.com/akd-io" target="_blank">
+                  GitHub
+                </a>
               </Button>
               <Button variant="outline" asChild>
-                <a href="https://www.linkedin.com/in/anders-damgaard/">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/anders-damgaard/" target="_blank">
+                  LinkedIn
+                </a>
               </Button>
             </div>
           </section>
